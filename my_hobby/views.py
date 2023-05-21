4839ca04b9ffbf6117e77ec22f6f2d7a042f4813
Bruce Chen <80359326+bruce18chen@users.noreply.github.com>
2023-05-21 09:12:10 -0400

    assignment 1 style change

--- my_hobby/views.py
+++ my_hobby/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 
 def index(request):
+    """
+    View function for the index page.
+    """
     return HttpResponse('Hello world!')
--- tests/my_hobby/test_views.py
+++ tests/my_hobby/test_views.py
@@ -1,6 +1,9 @@
 # test_views.py
-
 def test_index_ok(client):
+    """
+    Test case for the index view when a request is made with the Django test client.
+    It verifies that the response status code is 200 (OK).
+    """
     # Make a GET request to / and store the response object
     # using the Django test client.
     response = client.get('/')
